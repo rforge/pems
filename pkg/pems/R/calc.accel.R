82a6923db09428279dcb41e7008f6acf0153be29
karl <karl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-09-25 15:24:50 +0000

    updated functions
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/pems@28 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/pems/R/calc.accel.R
+++ pkg/pems/R/calc.accel.R
@@ -1,11 +1,11 @@
-calc.accel <-
+calc.accel <- 
 function(pems,
-		speed = velocity, time = local.time,
-		fun.method = "standard.accel",
-		fun.args = NULL,
-		vis.fun = TRUE,
-		fun.output = "pems"
-		){
+  speed = velocity, time = local.time,
+  fun.method = "standard.accel",
+  fun.args = NULL,
+  vis.fun = TRUE,
+  fun.output = "pems"
+){
 
 ###############
 #calculate accel 
@@ -13,79 +13,92 @@ function(pems,
 #source
 #karl (15/03/2009)
 
+###############
+#NOTES
+###############
+#currently only has a standard.accel method
+#assumes time is a count in seconds
+#currently allows only fun.args$dt which forces time interval 
+#currently creates accel.numeric if generated accel not unique
 
 ###############
 #TO DO
 ###########
-#currently makes no use of fun.method, fun.args
-#assumes time is a count in seconds
-#
+#rationalise vis.fun using lattice
 
 require(lattice)
 
-
 #for non-pems use
 if(is(pems)[1]=="pems") {
-	if(is.na(match(deparse(substitute(speed)), names(pems$data)))) {
-		stop(paste("\t calc.accel: '", as.character(substitute(speed)), "' not found in pems", sep = ""), call. = FALSE, domain = NA)
-	} 	
-	data <- pems$data
+  data <- pems$data
 } else {
-	if(is.na(match(deparse(substitute(speed)), names(pems)))) {
-		stop(paste("\t calc.accel: '", as.character(substitute(speed)), "' not found in (non-)pems", sep = ""), call. = FALSE, domain = NA)
-	} 	
-	data <- pems
+  data <- pems
 }
 
-this.time <- data[, deparse(substitute(time))]
+#check inputs
+if(fun.method!="standard.accel") {
+  stop(paste(" mis-match in calc.accel \n\tcalculation method '", fun.method, "' not recognised", sep = ""), call. = FALSE)
+} 
+if(is.na(match(deparse(substitute(speed)), names(data)))) {
+  stop(paste(" mis-match in calc.accel \n\tspeed parameter '", substitute(speed), "' not found in '", substitute(pems), "'", sep = ""), call. = FALSE)
+} 
+if(is.na(match(deparse(substitute(time)), names(data))) & is.null(fun.args$dt)) {
+  stop(paste(" mis-match in calc.accel \n\ttime parameter '", substitute(time), "' not found in '", substitute(pems), "'", sep = ""), call. = FALSE)
+} 
+
+#setup variables
+if(is.null(fun.args$dt)){
+  this.time <- c(NA, diff(data[, deparse(substitute(time))])) 
+}else {
+  this.time <- fun.args$dt 
+}
 this.speed <- data[, deparse(substitute(speed))]
 this.speed.name <- as.character(substitute(speed))
 
-#####################################################
-#main routines
-
-#reset units
-# if it is a pems
+#if pems, reset using units
 this.accel.units <- "unknown"
 if(is(pems)[1]=="pems") {
-	this.unit <- as.character(pems$units[, this.speed.name])	
-	this.correction <- "not found"
-	if(this.unit=="km/h") { this.correction = 0.27777777778 }
-	if(this.unit=="m/s") { this.correction = 1 }
-	if(this.correction=="not found") {
-		stop(paste("\t ca: ", this.speed.name, " units not recognised", sep=""), call. = FALSE, domain = NA)
-	}
-	this.accel.units <- "m/s/s"
-	this.speed <- this.speed * this.correction
+  this.unit <- as.character(pems$units[, this.speed.name])	
+  this.correction <- "not found"
+  if(this.unit=="km/h") { this.correction = 0.27777777778 }
+  if(this.unit=="m/s") { this.correction = 1 }
+  if(this.correction=="not found") {
+    stop(paste(" mis-match in calc.accel \n\t", this.speed.name, " units not recognised", sep=""), call. = FALSE, domain = NA)
+  }
+  this.accel.units <- "m/s/s"
+  this.speed <- this.speed * this.correction
 }
 
-this.time <- c(NA, diff(this.time))
-	#this.time <- c(this.time[1], this.time)
 this.accel <- c(0, diff(this.speed)) / this.time
-this.accel <- data.frame(this.accel)
-names(this.accel) <- "accel"
+this.accel <- data.frame(accel = this.accel)
 data <- cbind(data, this.accel)
-        
+names(data) <- make.unique(names(data))
+
 #setup pems info
 if(is(pems)[1]=="pems") {
-	pems$history <- c(pems$history, paste("calc.accel: accel generated using '", this.speed.name, "'", sep = ""))
-	pems$units[length(pems$units) + 1] <- this.accel.units
-	names(pems$units)[length(pems$units)] <- "accel"
+  this.history <- paste("calc.accel: ", names(data)[length(data)], " generated using '", this.speed.name, "'", sep = "")
+  if(!is.null(fun.args$dt)) { 
+    this.history <- paste(this.history, " and fun.args$dt = ", fun.args$dt, sep = "") 
+  } else {
+    this.history <- paste(this.history, " and '", substitute(time), "'", sep = "") 
+  }
+  pems$history <- c(pems$history, this.history)
+  pems$units[length(pems$units) + 1] <- this.accel.units
+  names(pems$units)[length(pems$units)] <- names(data)[length(data)] 
 }
         
 #plot if required
 if(vis.fun){
-	plot(this.speed,type="l")
-	lines(this.accel,col="blue")
+  plot(this.speed,type="l")
+  lines(this.accel,col="blue")
 }
 
+#repack and exit
 if(is(pems)[1]=="pems") {
-        pems$data <- data
-        pems
+  pems$data <- data
+  pems
 } else {
-        pems <- data
+  pems <- data
 }
-
 if(fun.output=="pems") { pems } else { this.accel }
 }
-
